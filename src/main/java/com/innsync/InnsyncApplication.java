88892107f639af2dc407d695af97d3e0b1bdfd98
Keerthana Krishnamurthy <krishnamurthykeerthana99@gmail.com>
2026-08-18 17:47:19 -0400

    Initial Spring Boot setup with PostgreSQL configuration

---
+++ src/main/java/com/innsync/InnsyncApplication.java
@@ -0,0 +1,13 @@
+package com.innsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnsyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/innsync/InnsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
