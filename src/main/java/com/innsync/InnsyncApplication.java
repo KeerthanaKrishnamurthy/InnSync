c2165706ea95d7cc6b2132f734980074029bb4b7
Keerthana Krishnamurthy <krishnamurthykeerthana99@gmail.com>
2026-08-26 19:45:33 -0700

    Added the same print message

--- src/main/java/com/innsync/InnsyncApplication.java
+++ src/main/java/com/innsync/InnsyncApplication.java
@@ -8,7 +8,7 @@ public class InnsyncApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InnsyncApplication.class, args);
-		System.out.println("Hello from main");
+		System.out.println("Hello from feature branch");
 	}
 
 }
