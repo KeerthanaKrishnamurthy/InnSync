51617c0995224fd03fa909e3012484d6d2bca8b8
Keerthana Krishnamurthy <krishnamurthykeerthana99@gmail.com>
2026-09-14 18:58:03 -0700

    Commit 3

--- src/main/java/com/innsync/InnsyncApplication.java
+++ src/main/java/com/innsync/InnsyncApplication.java
@@ -11,7 +11,7 @@ public class InnsyncApplication {
 		System.out.println("Hello from main branch");
 		System.out.println("Welcome to our application!");
 
-		System.out.println("Commit 2 Without Errors");
+		System.out.println("Commit 3 With Errors");
 	}
 
 }
